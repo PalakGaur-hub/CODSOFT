bfd7b13877b6ae4ff9103215eb61b33ed2535dab
PalakGaur-hub <p30669422@gmail.com>
2025-07-15 22:11:16 +0530

    arithmetic operation

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ elif choice == '3':
     print("Result:", result)
 elif choice == '4':
     if num2 != 0:
-        result = num1 / num2
+        result = num1 / num2 
         print("Result:", result)
     else:
         print("Error: Division by zero is not allowed.")
